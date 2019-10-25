011601c219d3262994e06a67a80cf63a0580f2f9
bellawulan99 <bwulan99@gmail.com>
2019-10-25 17:20:23 +0700

    player jump, move kanan kiri

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpd;
+
+    [SerializeField]
+    float jumpPower;
+
+    Rigidbody2D myRigidbody2d;
+
+    void Awake()
+    {
+        myRigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myRigidbody2d.AddForce(Vector2.right * Input.GetAxis("Horizontal") * moveSpd);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        myRigidbody2d.AddForce(Vector2.up * jumpPower);
+    }
+}
